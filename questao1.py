4151d769d6cca56b8df41cf8155262748470d85c
Felipelimaadev <131311523+Felipelimaadev@users.noreply.github.com>
2024-09-06 01:16:20 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -1,13 +1,11 @@
-# questao2.py
+# questao1.py
 
-def fibonacci(n):
-    a, b = 0, 1
-    while a < n:
-        a, b = b, a + b
-    return a == n or n == 0
+INDICE = 13
+SOMA = 0
+K = 0
 
-numero = int(input("Digite um número para verificar na sequência de Fibonacci: "))
-if fibonacci(numero):
-    print(f"{numero} pertence à sequência de Fibonacci.")
-else:
-    print(f"{numero} não pertence à sequência de Fibonacci.")
+while K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+
+print(SOMA)
